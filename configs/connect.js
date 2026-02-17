f5fb71bdd70f529caac4dbcabdacbb9411da4396
MelMab <mellmabeza15@gmail.com>
2026-02-17 16:42:11 +0200

    connet.js change

--- configs/connect.js
+++ configs/connect.js
@@ -6,11 +6,13 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 12691, // Add this line!
   waitForConnections: true,
   connectionLimit: 10,
-   ssl: {
-    rejectUnauthorized: false // Required for Aiven free tier connections
-  }
+  ssl: {
+    rejectUnauthorized: false 
+  },
+  connectTimeout: 20000 // Added to prevent early timeouts
 });
 
 module.exports = pool.promise();
